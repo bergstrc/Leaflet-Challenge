87303cf42d1e18cbe172090ea67a5538a6ae406c
Cole Bergstrom <75327298+bergstrc@users.noreply.github.com>
2021-04-19 23:26:31 -0400

    Update config.js

--- Mapping_HW/StarterCode/static/js/config.js
+++ Mapping_HW/StarterCode/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = 'pk.eyJ1IjoiY2hiZXJnc3Ryb20iLCJhIjoiY2tuNnM4d3NuMGd1bzJvcmlxam1qMTgwMiJ9.CTXzgA2jdqJEk8hZkNKkfw';
+const API_KEY = "pk.eyJ1IjoiY2hiZXJnc3Ryb20iLCJhIjoiY2tucGN0dzcxMDFmMDJvcXB4MjVwb3UybCJ9.PKOeNL3Eb8FcX1g0GHcpLA";
